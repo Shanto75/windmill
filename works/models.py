b8ff854e836eb6c4a06fbbe28bb7981d8eae6e42
Moniruzzaman Shanto <42086179+Shanto75@users.noreply.github.com>
2022-12-17 00:46:13 +0600

    updated about

--- works/models.py
+++ works/models.py
@@ -4,3 +4,6 @@ from django.db import models
 
 class About(models.Model):
     about = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.about
--- works/views.py
+++ works/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from works.models import About
+from django.contrib import messages
 # Create your views here.
 
 
@@ -8,6 +9,7 @@ def index(request):
     context = {
         'title': 'Welcome to home page'
     }
+    # messages.success(request, 'Profile details updated.')
     return render(request, 'index.html', context)
     # return HttpResponse('this is home page')
 
@@ -17,9 +19,13 @@ def about(request):
         aboutinfo = request.POST.get('about')
         about = About(about=aboutinfo)
         about.save()
+        messages.success(request, 'About Updated Successfully.')
 
+    abouts = About.objects.all()
+    
     context = {
-        'title': 'About Us'
+        'title': 'About Us',
+        'abouts': abouts
     }
     return render(request, 'about.html', context)
     # return HttpResponse('this is about page')
